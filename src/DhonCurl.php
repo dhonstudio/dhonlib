7bd2e7ed0bfb41b9dc10bb1bb2fe7a2ebd0e1abc
dhonstudio <doon13@gmail.com>
2023-02-01 12:56:23 +0700

    add curl put request

--- src/DhonCurl.php
+++ src/DhonCurl.php
@@ -39,4 +39,20 @@ class DhonCurl
 
         return json_decode($result, true);
     }
+
+    public function put($url, $data)
+    {
+        array_push($this->headers, 'Content-Type: application/json');
+        array_push($this->headers, 'Content-Length: ' . (string) strlen(json_encode($data)));
+
+        $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $this->headers);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT");
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+        $result = curl_exec($ch);
+        curl_close($ch);
+
+        return json_decode($result, true);
+    }
 }
